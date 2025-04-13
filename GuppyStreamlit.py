91180c5a038676956f65cec6a3b6b744f41d0e9c
ZeovantCapital <zeovantcapital@gmail.com>
2025-04-13 10:27:18 -0400

    Update GuppyStreamlit.py

--- GuppyStreamlit.py
+++ GuppyStreamlit.py
@@ -9,15 +9,15 @@ def main():
     # Create columns for side-by-side images
     col1, col2 = st.columns(2)
     with col1:
-        st.image("pet1.jpg", use_column_width=True)
+        st.image("images/cat1.jpg", use_column_width=True)
     with col2:
-        st.image("pet2.jpg", use_column_width=True)
+        st.image("images/cat2", use_column_width=True)
 
     col3, col4 = st.columns(2)
     with col3:
-        st.image("pet3.jpg", use_column_width=True)
+        st.image("images/cat3", use_column_width=True)
     with col4:
-        st.image("pet4.jpg", use_column_width=True)
+        st.image("images/cat4", use_column_width=True)
 
 
 if __name__ == "__main__":
